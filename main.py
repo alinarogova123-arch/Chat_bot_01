495931e6f7664a18b5bbfc7ae7135de44d97648e
alinarogova123-arch <alinarogova123@gmail.com>
2026-05-03 22:05:45 +0300

    change variable name

--- main.py
+++ main.py
@@ -29,10 +29,10 @@ def run_bot(headers, params, url, bot, chat_id):
                     link = message.get("lesson_url")
                     title = message.get("lesson_title")
                     if message.get("is_negative"):
-                        result = "К сожалению, в работе нашлись ошибки."
+                        review = "К сожалению, в работе нашлись ошибки."
                     else:
-                        result = "Преподавателю все понравилось, можно приступать к следующему уроку!"
-                    text = f"У вас проверили работу  <<{title}>>\n\n{result}\n\n{link}"
+                        review = "Преподавателю все понравилось, можно приступать к следующему уроку!"
+                    text = f"У вас проверили работу  <<{title}>>\n\n{review}\n\n{link}"
                     bot.send_message(chat_id=chat_id, text=text)
             params = {
                 "timestamp": timestamp_to_request
